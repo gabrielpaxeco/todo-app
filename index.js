a63d08ee613591d7257acb6bd147e356e069434f
Gabriel <gabrielenaty0923@gmail.com>
2023-11-26 23:57:43 -0300

    salvando tarefa no banco de dados

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+app.use(express.urlencoded({
+    extended:true
+}))
+
+app.post('/criar', (requisicao, resposta) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+        INSERT INTO tarefas(descicao, completa)
+        VALUES ('${descricao}', '${completa}')
+    `
+
+    conexao.query(sql, (erro)=>{
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.get('/', (requisicao, resposta)=>{
     resposta.render('home')
 })
